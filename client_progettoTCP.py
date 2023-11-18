53df52890596188595cd9d084389ee60bd47f485
DevIDEVerdino <145904069+DevIDEVerdino@users.noreply.github.com>
2023-11-18 21:16:29 +0100

    Update client_progettoTCP.py

--- client_progettoTCP.py
+++ client_progettoTCP.py
@@ -4,30 +4,25 @@ import facilities as fa
 
 if __name__ == "__main__":
     HOST = 'localhost'    # Il nodo remoto, qui metti il tuo indirizzo IP per provare connessione server e client dalla tua macchina alla tua macchina
-    PORT = 50010             # La stessa porta usata dal server
+    PORT = 50013             # La stessa porta usata dal server
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
 
     while True:
         data = s.recv(1024)
-        #print("ho ricevuto i dati:", data)
-        #lista = fa.bytes_to_list(data)
-        """
-        if lista == type(list):
-            print("ho identificato una lista")
-            print('Received: ', lista)
-        else:
-        """
+
         if type(fa.bytes_to_list(data)) is list:
-            #print("ho identificato una lista")
-            print('Received: ', fa.bytes_to_list(data))
+            data = fa.bytes_to_list(data)
+            #print('Received: ', data)
+            for i in range (0, len(data)):
+                print("#################################")
+                for k in range(0, len(data[i])):
+                    print(data[i][k])
             continue
         else:
-            #print("ho identificato una stringa")
             print('Received: ', data.decode())
-            if data == "chiusura della connessione":
+            if "chiusura della connessione" in data.decode():
                 break
-
 
 
         testo = input(">").encode()
